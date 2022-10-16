afe199782fecded233fb8621049d5e04c0c59b98
Victor Condino <un1tz3r0@gmail.com>
2022-10-16 10:33:45 -0400

    fixups to imports

--- k_diffusion/__init__.py
+++ k_diffusion/__init__.py
@@ -1,2 +1,3 @@
-from . import augmentation, config, evaluation, external, gns, layers, models, sampling, utils
+from . import (augmentation, config, evaluation, external, gns, layers, models,
+               sampling, utils)
 from .layers import Denoiser
